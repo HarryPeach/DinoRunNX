8239d118e37c32fbd63a718771b11d5661f26f78
Harry Peach <4750998+HarryPeach@users.noreply.github.com>
2022-04-14 16:21:49 +0100

    Update to work on the latest atmosphere/libnx versions

--- source/main.c
+++ source/main.c
@@ -15,7 +15,15 @@ int main(int argc, char *argv[])
 
     printf(CONSOLE_GREEN "Chromium Dino Run\n" CONSOLE_WHITE);
     printf("Ported by Harry Peach\n");
-    printf("\nLicensed under the BSD3 license, found in the LICENSE file.");
+    printf("\nLicensed under the BSD3 license, found in the LICENSE file.\n");
+    printf("\n Debug output:\n");
+
+    // Configure our supported input layout: a single player with standard controller styles
+    padConfigureInput(1, HidNpadStyleSet_NpadStandard);
+
+    // Init default gamepad
+    PadState pad;
+    padInitializeDefault(&pad);
 
     consoleUpdate(NULL);
 
@@ -23,43 +31,44 @@ int main(int argc, char *argv[])
     while (appletMainLoop())
     {
         // Scan all the inputs. This should be done once for each frame
-        hidScanInput();
+        padUpdate(&pad);
 
-        u64 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
+        u64 kDown = padGetButtonsDown(&pad);
 
         WebCommonConfig config;
         WebCommonReply reply;
         WebExitReason exitReason = 0;
 
         rc = webOfflineCreate(&config, WebDocumentKind_OfflineHtmlPage, 0, ".htdocs/dino_run/index.html");
-        printf("webOfflineCreate(): 0x%x\n", rc);
+        printf("webOfflineCreate() result: 0x%x\n", rc);
 
         if (R_SUCCEEDED(rc))
         {
             rc = webConfigSetFooterFixedKind(&config, WebFooterFixedKind_Hidden);
-            printf("webConfigSetFooterFixedKind(): 0x%x\n", rc);
+            printf("webConfigSetFooterFixedKind() result: 0x%x\n", rc);
 
             if (R_SUCCEEDED(rc))
             { // Launch the applet and wait for it to exit.
                 rc = webConfigShow(&config, &reply);
                 printf("webConfigShow(): 0x%x\n", rc);
-                if(rc == 0x5d59){ // Warn the user when it appear they havent overridden a title to run the game
+                if (rc == 0x5d59)
+                { // Warn the user when it appear they havent overridden a title to run the game
                     printf(CONSOLE_RED "!!! There was an issue showing the browser window. Make sure you're running the game through an overridden title and not the album. Hold KEY_PLUS to exit.\n" CONSOLE_WHITE);
                 }
-            }
 
-            if (R_SUCCEEDED(rc))
-            { // Normally you can ignore exitReason.
                 rc = webReplyGetExitReason(&reply, &exitReason);
                 printf("webReplyGetExitReason(): 0x%x", rc);
                 if (R_SUCCEEDED(rc))
                     printf(", 0x%x", exitReason);
                 printf("\n");
+                consoleUpdate(NULL);
+                svcSleepThread(1000000000ull);
                 break;
+                // continue;
             }
         }
 
-        if(kDown & KEY_PLUS)
+        if (kDown & HidNpadButton_Plus)
             break;
 
         // Update the console, sending a new frame to the display
